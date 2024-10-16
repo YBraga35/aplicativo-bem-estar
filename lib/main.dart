08754e86acd6ba6fcecb703806038c674c24eaad
Caio Foti Pontes <caio.pontes@ufcspa.edu.br>
2024-10-16 15:12:41 -0300

    Atualização das Alterações do Eduardo pt2

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // Definindo a tela de boas-vindas como a tela inicial
-      home: BoasVindas(),
+      home: const BoasVindas(),
     );
   }
 }
