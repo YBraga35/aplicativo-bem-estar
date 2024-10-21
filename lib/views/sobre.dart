e1270fc115906e24c85ddac3f434cd7146e10d75
Fortes <mrlorenzo1608@gmail.com>
2024-10-21 19:20:31 -0300

    Alteracoes

--- lib/views/sobre.dart
+++ lib/views/sobre.dart
@@ -11,7 +11,7 @@ class Sobre extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sobre'),
+        title: const Text('SOBRE TESTE'),
         backgroundColor: const Color(0xFF448D9C),
       ),
       body: SingleChildScrollView(
