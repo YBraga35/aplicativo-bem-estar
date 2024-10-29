9adf290b4c473266b75eb045a70dfd0254c9100a
Eduardo Eifler <eduardoeiflerf@gmail.com>
2024-10-29 16:12:02 -0300

    atualização da tela de seleção de trilhas, com botões responsivos que alteram de cor ao serem selecionados

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
+<<<<<<< HEAD
 export "FLUTTER_ROOT=C:\Users\Caio\dev\flutter"
 export "FLUTTER_APPLICATION_PATH=C:\Users\Caio\Documents\vsc_directory\EngenhariaSoftwareII\projeto_zenjourney\aplicativo-bem-estar"
+=======
+export "FLUTTER_ROOT=C:\Users\Eduardo\Downloads\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Eduardo\Documents\GitHub\aplicativo-bem-estar"
+>>>>>>> ea28821 (atualização da tela de seleção de trilhas, com botões responsivos que alteram de cor ao serem selecionados)
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
